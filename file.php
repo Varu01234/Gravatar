a676ab0d88d79dfde823fc8a3381d1ab6d0a023d
varu01234 <varadhan198@gmail.com>
2013-07-11 21:06:58 +0530

    Update file.php

--- file.php
+++ file.php
@@ -1,10 +1,11 @@
+<html>
 <?php
         $email = "someone@someone.com";// your gmail
-  			$default = "http://en.gravatar.com/userimage/57896534/d81242696efc5614558714138e.jpg";//your default image url address
-				$size =200;//size of your image
-				$grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
+  	$default = "http://en.gravatar.com/userimage/57896534/d81242696efc5614558714138e.jpg";//your default image url address
+	$size =200;//size of your image
+	$grav_url = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
 ?>
-				<div style="color:white; ">
-				<img src="<?php echo $grav_url; ?>" alt="" />
-				</div>
-        
+	<div style="color:white; ">
+	<img src="<?php echo $grav_url; ?>" alt="" />
+	</div>
+</html>
